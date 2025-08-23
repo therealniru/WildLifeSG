d5723543063159f977ebd5858fac283c47fd7c1e
rahul <rahul.vijay.0406@gmail.com>
2025-08-23 20:17:08 +0800

    Remove Keys

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCpwQc6G4eWfXGO_TCBC8Pqn5Zv8mqzZaA",
-  authDomain: "wildlifesg-8d634.firebaseapp.com",
-  projectId: "wildlifesg-8d634",
-  databaseURL:  "https://wildlifesg-8d634-default-rtdb.asia-southeast1.firebasedatabase.app",
-  storageBucket: "wildlifesg-8d634.firebasestorage.app",
-  messagingSenderId: "67879099959",
-  appId: "1:67879099959:web:bc7fd18fc2f39e2b6ca9b2",
-  measurementId: "G-C4Z0VF953N"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  databaseURL:  "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
