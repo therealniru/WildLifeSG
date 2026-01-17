b315d7fea6ca981d467f46b816a54fa56706716f
rahul <rahul.vijay.0406@gmail.com>
2026-01-17 16:26:07 +0800

    Add necessary upgrades

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  databaseURL:  "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyCpwQc6G4eWfXGO_TCBC8Pqn5Zv8mqzZaA",
+  authDomain: "wildlifesg-8d634.firebaseapp.com",
+  databaseURL: "https://wildlifesg-8d634-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wildlifesg-8d634",
+  storageBucket: "wildlifesg-8d634.firebasestorage.app",
+  messagingSenderId: "67879099959",
+  appId: "1:67879099959:web:bc7fd18fc2f39e2b6ca9b2",
+  measurementId: "G-C4Z0VF953N"
 };
 
 // Initialize Firebase
